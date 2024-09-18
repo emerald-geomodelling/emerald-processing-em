78cf463fdcd8da4829a8292c54acb5e6807fe449
Benjamin R. Bloss <bloss.benjamin@gmail.com>
2024-09-18 14:53:49 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='emeraldprocessing',
-    version='0.1.4',
+    version='0.1.5',
     description="""Processing tools for multi-sounding EM geophysical data""",
     long_description="""Processing tools and and workflow for multi-sounding EM geophysical data.""",
     long_description_content_type="text/markdown",
